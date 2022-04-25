bcb20d3e44895c9e73be30209bce532cb59def62
Noah <Noahpbrinkman@gmail.com>
2022-04-25 17:08:56 +0200

    Full on project push

---
+++ Project-Final-Approach-1.0.0/MyGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Buffers.Text;
+
+namespace GXPEngine;
+
+public class MyGame : Game
+{
+    public MyGame() : base(800, 600, false)
+    {
+        
+    }
+    public static void Main(){
+        new MyGame().Start();
+    }
+}
+
