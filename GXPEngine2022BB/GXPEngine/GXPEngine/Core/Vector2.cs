2e8af59f028f15143b2e1e2ab21fc8e9f2a499c0
TygoDev <tygohaan@gmail.com>
2022-04-26 11:33:32 +0200

    Name change/basic script/changes
    Gonna work on making a basic airplane

--- GXPEngine2022BB/GXPEngine/GXPEngine/Core/Vector2.cs
+++ GXPEngine2022BB/GXPEngine/GXPEngine/Core/Vector2.cs
@@ -2,20 +2,36 @@ using System;
 
 namespace GXPEngine.Core
 {
-	public struct Vector2
-	{
-		public float x;
-		public float y;
-		
-		public Vector2 (float x, float y)
-		{
-			this.x = x;
-			this.y = y;
-		}
-		
-		override public string ToString() {
-			return "[Vector2 " + x + ", " + y + "]";
-		}
-	}
+    public struct Vector2
+    {
+        public float x;
+        public float y;
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static float Distance(Vector2 a, Vector2 b)
+        {
+            return Mathf.Sqrt(Mathf.Pow(b.x - a.x, 2) + Mathf.Pow(b.y - a.y, 2));
+        }
+
+        override public string ToString()
+        {
+            return "[Vector2 " + x + ", " + y + "]";
+        }
+
+        public static Vector2 operator +(Vector2 a, Vector2 b) => new Vector2(a.x + b.x, a.y + b.y);
+
+        public static Vector2 operator -(Vector2 a, Vector2 b) => new Vector2(a.x - b.x, a.y - b.y);
+
+        public static Vector2 operator -(Vector2 a) => new Vector2(-a.x, -a.y);
+
+        public static Vector2 operator *(Vector2 a, float b) => new Vector2(a.x * b, a.y * b);
+
+        public static Vector2 operator *(Vector2 a, Vector2 b) => new Vector2(a.x * b.x, a.y * b.y);
+    }
 }
 
---
+++ GXPEngine2022BB/GXPEngine/Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GXPEngine;
+using GXPEngine.Core;
+
+public class Square : Sprite
+{
+
+    public Square() : base("square.png")
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
